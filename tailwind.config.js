31e87771c143eb0c1d1ce6fdb0dfd72b46e0606f
oonderrr <onder.bugdaci@gmail.com>
2024-02-01 15:57:33 +0300

    Nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container: {
+      center : true,
+    },
+
+    screens:{
+      lg: "1140px",
+      xl: "1140px",
+      "2xl": "1140px"
+    },
+
+    extend: {
+      fontFamily : {
+        gemunu : ['Gemunu Libre', "sans-serif"],
+        openSans : ['Open Sans', "sans-serif"]
+      },
+
+      colors:{
+        'gega-red' : '#BC1A45',
+        "gega-melon" : "#FFD369",
+        "gega-gery" : "#DDDDDD",
+        "gega-white" : "#F7F7F7"
+      },
+    },
   },
   plugins: [],
 }
